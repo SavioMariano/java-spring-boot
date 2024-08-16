a3e27a1098a2749cb0840c6493bbe0ec9e731c0d
Savio <saviomariano77@gmail.com>
2024-08-16 11:07:15 -0300

    removendo projeto duplicado de JPA de dentro do primeiros passos

--- primeiros-passos/aula-string-data-jpa/src/main/java/dio/aula_string_data_jpa/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dio.aula_string_data_jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import dio.aula_string_data_jpa.model.Users;
-
-public interface UserRepository extends JpaRepository<Users, Integer>{
-
-}
